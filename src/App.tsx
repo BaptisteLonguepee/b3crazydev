1713f1238332f3788f1cee4c76657a01c6c9ef56
Baptiste <batbox.longuepee@gmail.com>
2023-09-19 21:01:20 +0200

    Ajout Son

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import HomePages from "./pages/HomePage/HomePage";
 
 function App() {
     const balloonElement = useRef<HTMLDivElement>(null);
-    const explosionSound = useRef<HTMLAudioElement>(null); // Référence pour l'élément audio
+    const explosionSound = useRef<HTMLAudioElement>(null);
+    const explosionBallon = useRef<HTMLAudioElement>(null);
+
     const [ballonExploded, setBallonExploded] = useState(false); // État pour vérifier si le ballon a explosé ou non
     const VOLUME_THRESHOLD = 50;
     const MAX_SIZE = 15;
     let balloonSize = 1;
 
     useEffect(() => {
+
         function inflateBalloon() {
             balloonSize += 0.05;
             if (balloonElement.current) {
@@ -19,16 +22,6 @@ function App() {
 
             if (balloonSize > MAX_SIZE) {
                 explodeBalloon();
-            }
-        }
-
-        function explodeBalloon() {
-            if (balloonElement.current) {
-                balloonElement.current.classList.add('explode');
-                explosionSound.current?.play(); // Jouer le son d'explosion
-                setTimeout(() => {
-                    setBallonExploded(true);
-                }, 500);
             }
         }
 
@@ -63,11 +56,24 @@ function App() {
 
     }, []);  // Le tableau vide signifie que useEffect ne s'exécute qu'au montage du composant
 
+    function explodeBalloon() {
+        if (balloonElement.current) {
+            balloonElement.current.classList.add('explode');
+            // Jouer le son d'explosion
+            explosionBallon.current?.play();
+            explosionSound.current?.play();
+
+            setTimeout(() => {
+                setBallonExploded(true);
+            }, 500);
+        }
+    }
+
     return (
         <div className="container">
             {ballonExploded ? <HomePages /> : <div className="ballon" ref={balloonElement}></div>}
+            <audio ref={explosionBallon} src="/BallonPop.mp3" preload="auto"></audio>
             <audio ref={explosionSound} src="/SUIII.mp3" preload="auto"></audio>
-            {/* Assurez-vous de remplacer "path_to_your_explosion_sound.mp3" par le chemin d'accès réel vers votre fichier audio */}
         </div>
     );
 
