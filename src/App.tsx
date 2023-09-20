766191094355e2f70e5616d0fb31f3d21bde9fa8
Vinybsqn <vianneybasquin59@gmail.com>
2023-09-20 11:35:09 +0200

    rectif refresh

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,15 @@ function App() {
     const explosionSound = useRef<HTMLAudioElement>(null);
     const explosionBallon = useRef<HTMLAudioElement>(null);
 
-    const [ballonExploded, setBallonExploded] = useState(false); // État pour vérifier si le ballon a explosé ou non
+    const [ballonExploded, setBallonExploded] = useState(() => {
+        return localStorage.getItem('ballonExploded') === 'true';
+    });
+
     const VOLUME_THRESHOLD = 50;
     const MAX_SIZE = 15;
     let balloonSize = 1;
 
     useEffect(() => {
-
         function inflateBalloon() {
             balloonSize += 0.05;
             if (balloonElement.current) {
@@ -27,46 +29,48 @@ function App() {
             }
         }
 
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(stream => {
-                const audioContext = new AudioContext();
-                const analyser = audioContext.createAnalyser();
-                const source = audioContext.createMediaStreamSource(stream);
-                source.connect(analyser);
+        if (!ballonExploded) {
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then(stream => {
+                    const audioContext = new AudioContext();
+                    const analyser = audioContext.createAnalyser();
+                    const source = audioContext.createMediaStreamSource(stream);
+                    source.connect(analyser);
 
-                const dataArray = new Uint8Array(analyser.frequencyBinCount);
+                    const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-                function checkVolume() {
-                    analyser.getByteFrequencyData(dataArray);
-                    let sum = 0;
-                    for (let i = 0; i < dataArray.length; i++) {
-                        sum += dataArray[i];
+                    function checkVolume() {
+                        analyser.getByteFrequencyData(dataArray);
+                        let sum = 0;
+                        for (let i = 0; i < dataArray.length; i++) {
+                            sum += dataArray[i];
+                        }
+                        let average = sum / dataArray.length;
+                        if (average > VOLUME_THRESHOLD) {
+                            inflateBalloon();
+                        }
+                        requestAnimationFrame(checkVolume);
                     }
-                    let average = sum / dataArray.length;
-                    if (average > VOLUME_THRESHOLD) {
-                        inflateBalloon();
-                    }
-                    requestAnimationFrame(checkVolume);
-                }
 
-                checkVolume();
+                    checkVolume();
 
-            })
-            .catch(error => {
-                console.error('Erreur d’accès au micro:', error);
-            });
+                })
+                .catch(error => {
+                    console.error('Erreur d’accès au micro:', error);
+                });
+        }
 
-    }, []);  // Le tableau vide signifie que useEffect ne s'exécute qu'au montage du composant
+    }, [ballonExploded]);
 
     function explodeBalloon() {
         if (balloonElement.current) {
             balloonElement.current.classList.add('explode');
-            // Jouer le son d'explosion
             explosionBallon.current?.play();
             explosionSound.current?.play();
 
             setTimeout(() => {
                 setBallonExploded(true);
+                localStorage.setItem('ballonExploded', 'true');
             }, 500);
         }
     }
