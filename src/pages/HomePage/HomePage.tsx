516d16cae865d1aa79b4a49f09763b8dfc7e76b9
Baptiste <batbox.longuepee@gmail.com>
2023-09-20 16:41:56 +0200

    Quizz

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,24 +3,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-
 const HomePage: React.FC = () => {
     return (
-        <div className="home-container">
-            <div className="banner">
-                <img src="/banner2.png" alt="Bannière" className="banner-image"/>
-                <div className="banner-text">
-                    Footix Quiz
+        <>
+            <div className="home-container">
+                <div className="banner">
+                    <img src="/banner2.png" alt="Bannière" className="banner-image"/>
+                    <div className="banner-text">
+                        Footix Quiz
+                    </div>
                 </div>
+                <Link to="/quizz">
+                    <button className="quiz-button">
+                        Commencer le Quiz
+                    </button>
+                </Link>
             </div>
-            <Link to="/quizz">
-                <button className="quiz-button">
-                    Commencer le Quiz
-                </button>
-            </Link>
-        </div>
-
-
+        </>
     );
 }
 
