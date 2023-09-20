ac473f3009e7720b37081367ae2027d8e146b53b
Vinybsqn <vianneybasquin59@gmail.com>
2023-09-20 09:06:33 +0200

    HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,17 @@
 import './HomePage.css';
+import React from 'react';
 
-function HomePage(){
+const HomePage: React.FC = () => {
     return (
-        <div>Home Pageeeee</div>
+        <div className="container">
+            <div className="banner">
+                <img src="/banner2.png" alt="Bannière" className="banner-image"/>
+                <div className="banner-text">
+                    Footix Quiz
+                </div>
+            </div>
+            {/* Autres éléments de la page d'accueil ici */}
+        </div>
     );
 }
 
